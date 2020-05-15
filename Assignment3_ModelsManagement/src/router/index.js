c7ffd4dacd3135cb17ca71ae6ffd203a55615a08
Celia <au582514@post.au.dk>
2020-05-15 21:08:42 +0200

    router

--- Assignment3_ModelsManagement/src/router/index.js
+++ Assignment3_ModelsManagement/src/router/index.js
@@ -1,5 +1,47 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Jobs from "../views/Jobs.vue";
+import Models from "../views/Models.vue";
+import Managers from "../views/Managers.vue";
 
 Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+
+    {
+        path: "/",
+        name: "Login",
+        component: Login
+    },
+    {
+        path: "/",
+        name: "Jobs",
+        component: Jobs
+    },
+    {
+        path: "/",
+        name: "Models",
+        component: Models
+    },
+    {
+        path: "/",
+        name: "Managers",
+        component: Managers
+    }
+
+];
+
+const router = new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
+});
+
+export default router;

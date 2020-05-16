5ac11890519bec216ce67857750f371d7c2063b7
Celia <au582514@post.au.dk>
2020-05-16 14:13:05 +0200

    celia router

--- Assignment3_ModelsManagement/src/router/index.js
+++ Assignment3_ModelsManagement/src/router/index.js
@@ -1,42 +1,46 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-//import Home from "../views/Home.vue";
-//import Login from "../views/Login.vue";
-//import Jobs from "../views/Jobs.vue";
-//import Models from "../views/Models.vue";
-//import Managers from "../views/Managers.vue";
+import Home from "../components/Home.vue";
+//import Login from "../components/Login.vue";
+//import Jobs from "../components/Jobs.vue";
+//import Models from "../components/Models.vue";
+//import Managers from "../components/Managers.vue";
 
-//Vue.use(VueRouter);
+Vue.use(VueRouter);
 
-//const routes = [
-//    {
-//        path: "/",
-//        name: "Home",
-//        component: Home
-//    },
+export default new VueRouter({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    }
 
-//    {
-//        path: "/",
-//        name: "Login",
-//        component: Login
-//    },
-//    {
-//        path: "/",
-//        name: "Jobs",
-//        component: Jobs
-//    },
-//    {
-//        path: "/",
-//        name: "Models",
-//        component: Models
-//    },
-//    {
-//        path: "/",
-//        name: "Managers",
-//        component: Managers
-//    }
+    //{
+    //    path: "/login",
+    //    name: "Login",
+    //    component: Login
+    //},
+    //{
+    //    path: "/jobs",
+    //    name: "Jobs",
+    //    component: Jobs
+    //},
+    //{
+    //    path: "/models",
+    //    name: "Models",
+    //    component: Models
+    //},
+    //{
+    //    path: "/managers",
+    //    name: "Managers",
+    //    component: Managers
+    //}
 
-//];
+    ]
+})
 
 //const router = new VueRouter({
 //    mode: "history",

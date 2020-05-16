b77c19fd38d1702cd461ad1a61785a0b7746a314
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2020-05-16 09:19:27 +0200

    Fetch login

--- Assignment3_ModelsManagement/src/components/Home.vue
+++ Assignment3_ModelsManagement/src/components/Home.vue
@@ -1,30 +1,40 @@
 <template>
     <div class="home">
         <h1>{{ msg }}</h1>
-        <p>Welcome to ModelsManagement!</p>
+        <p>Welcome to ModelsManagement. Please log in!</p>
     </div>
 </template>
 
 <script>
+    // Dette skal ændres til gyldige logins!
+    let username = "Poul"
+    let password = 123
     export default {
         name: 'Home',
         props: {
             msg: String
-        }
-        fetch('/api/Account/login', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: username,
-                password: password
-            }),
-                headers: new Headers({
-                    'Content-Type': 'application/json'
-                })
-}).then(res => res.json().then((token) => {
+        },
+
+        methods: {
+            async login() {
+                fetch('http://localhost:1337/api/Account/login', {
+                    method: 'POST',
+                    body: JSON.stringify(
+                        {
+                            email: username,
+                            password: password
+                        }),
+                    headers: new Headers({
+                        'Content-Type': 'application/json'
+                    })
+                }).then(res => res.json()).then((token) => {
                     localStorage.setItem("token", token.jwt);
-)
+                })
                     .catch(error => alert('Error:', error))
-    };
+            },
+        },
+    }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
